31d32635031c65cfdfb4145791201d3676d60bea
Samuel12372 <c2028335@hallam.shu.ac.uk>
2025-02-06 15:25:17 +0000

    Update MCQ.js
    multiple choice question component

--- frontend/src/components/MCQ.js
+++ frontend/src/components/MCQ.js
@@ -1,21 +1,45 @@
 import { Button, Card } from "antd";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import '../CSS/Questions.css';
 
-function MultipleChoice() {
-    useEffect(() => {
-        
-    }, []);
 
-    const handleChoiceClick = () => {};
+
+function MultipleChoice() {
+   
+  const [quiz , setQuiz] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  useEffect(() => {
+      axios.get(`http://localhost:8080/quiz/67a49e674c58fb1b231ea574`)
+      .then((response) => {
+          setQuiz(response.data);
+          setQuestions(response.data.questions);
+          setCurrentQuestion(0);
+      })
+      .catch((error) => {
+          console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+
+  const handleChoiceClick = () => {
+    console.log('Choice Clicked');
+    //setCurrentQuestion(currentQuestion + 1);
+  };
 
   return (
     <div className="multiplechoice">
-        <Card title= "question" id="MCQCard">
-            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{}</Button>
-            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{}</Button>
-            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{}</Button>
+      {questions.length > 0 && (
+        <Card title={questions[currentQuestion].questionText} id="MCQCard">
+
+            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{questions[currentQuestion].options[0]}</Button>
+            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{questions[currentQuestion].options[1]}</Button>
+            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{questions[currentQuestion].options[2]}</Button>
+            <Button onClick={handleChoiceClick} type="primary" id="ChoiceButton">{questions[currentQuestion].options[3]}</Button>
         </Card>
+      )}
     </div>
   );
 }
