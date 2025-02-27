16b1d3a80a0242aee87bb3025d314841b3ef362c
Samuel12372 <c2028335@hallam.shu.ac.uk>
2025-02-27 15:06:41 +0000

    css

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -4,9 +4,16 @@ import "../CSS/Navbar.css";
 
 const Title = () => {
     return (
-        <div className="title">
-            <h3>CodeClash</h3>
+        <>
+        <div className="glitch-container">
+            <div className="code">
+                <h3>Code</h3>
+            </div>
+            <div className="clash">
+                <h3>Clash</h3>
+            </div>
         </div>
+        </>
     );
 };
 
